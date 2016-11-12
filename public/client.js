af66ba36a9942d1f350cdf7c58162dd771b32127
Sabrina Markon <sabrina@phpsitescripts.com>
2016-11-12 04:05:04 -0700

    Added download button for user to download the generated image.

--- public/client.js
+++ public/client.js
@@ -200,7 +200,9 @@ $(function() {
    if (pickimage !== 'none') {
       var canvascontainer = document.getElementById("canvascontainer");
       var imgstyle = "max-width: 100%; max-height: 100%; background: none;";
+      
       var newid = $("#canvascontainer > div").length + 1;
+      
     $('#canvascontainer').append($('<div id="' + newid + '" class="canvaslayer picture"><img class="ui-widget-content" src="' + pickimage + '" style="' + imgstyle + '"></div>')
         .draggable({ containment : "body" })
         .resizable({ 
@@ -263,10 +265,20 @@ $(function() {
                 theCanvas = canvas;
                 $('#savediv').append('<h3>Your Image:</h3>')
                 $('#savediv').append(canvas);
+                //Show the download button.
+                $('#downloadbuttondiv').show();
+                //Set hidden field's value to image data (base-64 string)
+                $('#img_val').val(canvas.toDataURL("image/png"));
             }
         });
     });
-        
+    
+    // DOWNLOAD IMAGE:
+    $("#downloadbutton").on('click', function() {
+      document.getElementById("downloadform").submit();
+    });
+    
+    
   // SUPPORTING FUNCTIONS: 
 
   // GET CONTRASTING TEXT COLOR FOR BACKGROUNDS:
